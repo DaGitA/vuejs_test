aed4c24e73dfe7ad2b9a72ea9bcf943e7d40e9f3
David Arel <david.arel@outlook.com>
2021-07-26 16:08:42 -0400

    Add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <img alt="logo" src="../assets/logo.png">
+  <h1>List</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
